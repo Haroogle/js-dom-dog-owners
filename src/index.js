196c3fedddf179c015ff6fa16161aed8a1052165
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-10-19 18:42:41 +0100

    added button: to-do is add change functionality

--- src/index.js
+++ src/index.js
@@ -26,30 +26,39 @@ data.forEach((dogJSObject) => {
   function dogDescription(Desc) {
     const dogContainer = document.querySelector(".main__dog-section");
     const dogBio = document.createElement("div");
+    dogBio.setAttribute("class", "main__dog-section__desc");
     dogContainer.appendChild(dogBio);
     const title = document.createElement("h3");
-    title.innerText = "Bio"; 
-    dogBio.appendChild (title)
-    const p = document.createElement("p")
-    p.innerText = dogJSObject.bio
-    dogBio.appendChild (p)
+    title.innerText = "Bio";
+    dogBio.appendChild(title);
+    const p = document.createElement("p");
+    p.innerText = dogJSObject.bio;
+    dogBio.appendChild(p);
   }
 
-    function goodBoyButton(goodboy) {
-      const dogContainer = document.querySelector(".main__dog-section__btn")
-      const dogButton = document.createElement("main__dog-section__btn")
-      dogContainer.appendChild(dogButton)
-}
-    
-    
-  
-
+  function goodBoyButton(goodboy) {
+    const dogContainer = document.querySelector(".main__dog-section");
+    const dogButtonSection = document.createElement("div");
+    dogButtonSection.setAttribute("class", "main__dog-section__btn");
+    dogContainer.appendChild(dogButtonSection);
+    const p = document.createElement("p");
+    const em = document.createElement("em");
+    em.innerText = "Is naughty?";
+    p.innerText = em.innerText + " " + IsItGoodBoy(dogJSObject);
+    dogButtonSection.appendChild(p);
+    const dogButton = document.createElement("button");
+    dogButton.innerText = "Good dog!";
+    dogButtonSection.appendChild(dogButton);
+  }
+  function IsItGoodBoy(dogJSObject) {
+    if (dogJSObject.isGoodDog === true) {
+      return "Yes!";
+    } else {
+      return "No!";
+    }
+  }
   dogsLi.addEventListener("click", handleDogselectName);
   dogsLi.addEventListener("click", insertDogPic);
   dogsLi.addEventListener("click", dogDescription);
   dogsLi.addEventListener("click", goodBoyButton);
-
-
 });
-
-
