ecf43325f77a43eee020f584e11eeab1553a84ea
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-10-19 16:27:27 +0100

    names and images added when tabs selected

--- src/index.js
+++ src/index.js
@@ -9,4 +9,21 @@ data.forEach((dogJSObject) => {
   dogsLi.setAttribute("class", "dogs-list__button");
   dogList.appendChild(dogsLi);
   dogsLi.innerText = dogJSObject.name;
+
+  function handleDogselectName(dogName) {
+    const dogContainer = document.querySelector(".main__dog-section");
+    dogContainer.innerHTML = "";
+    const name = document.createElement("h2");
+    name.innerText = dogJSObject.name;
+    dogContainer.appendChild(name);
+  }
+  function insertDogPic(image) {
+    const dogContainer = document.querySelector(".main__dog-section");
+    const dogPic = document.createElement("img");
+    dogPic.setAttribute("src", dogJSObject.image);
+    dogContainer.appendChild(dogPic);
+  }
+
+  dogsLi.addEventListener("click", handleDogselectName);
+  dogsLi.addEventListener("click", insertDogPic);
 });
