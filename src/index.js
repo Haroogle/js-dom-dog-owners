ac133703f57be6354cde21feedc4be4f097c1260
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-10-19 13:17:39 +0100

    dogs list added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+const dogList = document.querySelector(".dogs-list");
+console.log(dogList);
+data.forEach((dogJSObject) => {
+  console.log(dogJSObject);
+  const dogsLi = document.createElement("li");
+  dogsLi.setAttribute("class", "dogs-list__button");
+  dogList.appendChild(dogsLi);
+  dogsLi.innerText = dogJSObject.name;
+});
